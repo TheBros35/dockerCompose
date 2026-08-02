a3831584b8d69f7148b73869e0acc0bf79dbee5c
Brandon Ambrose <bpambrose97@gmail.com>
2026-08-02 17:30:24 -0400

    moved gitignore to root and fixed bug in sslcerts

--- haproxy/stats-with-https/ssl-cert-generation/sslcerts.sh
+++ haproxy/stats-with-https/ssl-cert-generation/sslcerts.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-openssl req -x509 -out ../certs1/server1.crt -keyout ../certs1/server1.key \
+openssl req -x509 -out ../web-nodes/certs1/server1.crt -keyout ../web-nodes/certs1/server1.key \
   -newkey rsa:4096 -nodes -sha512 -days 365 \
   -subj '/CN=webnode1' -extensions EXT -config <( \
    printf "[dn]\nCN=webnode1\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:webnode1\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
 
-openssl req -x509 -out ../certs2/server2.crt -keyout ../certs2/server2.key \
+openssl req -x509 -out ../web-nodes/certs2/server2.crt -keyout ../web-nodes/certs2/server2.key \
   -newkey rsa:4096 -nodes -sha512 -days 365 \
   -subj '/CN=webnode2' -extensions EXT -config <( \
    printf "[dn]\nCN=webnode2\n[req]\ndistinguished_name = dn\n[EXT]\nsubjectAltName=DNS:webnode2\nkeyUsage=digitalSignature\nextendedKeyUsage=serverAuth")
